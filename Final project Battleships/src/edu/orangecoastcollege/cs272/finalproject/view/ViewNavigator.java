b78691b17aec97956230c07eae63a9c302f4ca76
gundyne <dathuydoan@gmail.com>
2017-05-13 11:52:16 -0700

    Added code to ViewNavigator

--- Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/ViewNavigator.java
+++ Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/ViewNavigator.java
@@ -1,5 +1,29 @@
 package edu.orangecoastcollege.cs272.finalproject.view;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ViewNavigator {
+	public static Stage mainStage;
+
+	public static void setStage(Stage stage) {
+		mainStage = stage;
+	}
+
+	public static void loadScene(String title, String sceneFXML) {
+
+		try {
+			mainStage.setTitle(title);
+			Scene scene = new Scene(FXMLLoader.load(ViewNavigator.class.getResource(sceneFXML)));
+			mainStage.setScene(scene);
+			mainStage.show();
+		} catch (IOException e) {
+			System.err.println("Error loading: " + sceneFXML + "\n" + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 }
