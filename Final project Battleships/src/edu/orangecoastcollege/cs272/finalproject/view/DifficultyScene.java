99cbefff4691f5daa84bbef9f6dea1d11524b6fe
gundyne <dathuydoan@gmail.com>
2017-05-13 12:37:28 -0700

    Added DifficultyScene

---
+++ Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/DifficultyScene.java
@@ -0,0 +1,22 @@
+package edu.orangecoastcollege.cs272.finalproject.view;
+
+import edu.orangecoastcollege.cs272.finalproject.controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class DifficultyScene {
+
+	private static Controller controller = Controller.getInstance();
+	
+	@FXML
+	private ComboBox<String> difficultyCB;
+	
+	@FXML
+	private Button difficultyChooseBtn;
+	
+	@FXML
+	public Object difficultyClick(){
+		return null;
+	}
+}
