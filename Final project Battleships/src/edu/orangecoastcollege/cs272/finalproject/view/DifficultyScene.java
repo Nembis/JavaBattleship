4084323c488ab209569d2c4fac9c1584153ae613
gundyne <dathuydoan@gmail.com>
2017-05-18 19:29:03 -0700

    Made DifficultyScene initializable

--- Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/DifficultyScene.java
+++ Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/DifficultyScene.java
@@ -1,13 +1,19 @@
 package edu.orangecoastcollege.cs272.finalproject.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import edu.orangecoastcollege.cs272.finalproject.controller.Controller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
-public class DifficultyScene {
+public class DifficultyScene implements Initializable {
 
-	private static Controller controller = Controller.getInstance();
+	//private static Controller controller = Controller.getInstance();
 	
 	@FXML
 	private ComboBox<String> difficultyCB;
@@ -18,5 +24,15 @@ public class DifficultyScene {
 	@FXML
 	public Object difficultyClick(){
 		return null;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		ObservableList<String> difficulties = FXCollections.observableArrayList();
+		difficulties.add("Easy");
+		difficulties.add("Normal");
+		difficulties.add("Hard");
+		difficultyCB.setItems(difficulties);
+		difficultyCB.getSelectionModel().select("Easy");
 	}
 }
