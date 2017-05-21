39d484095f01c883f922a5f6c1d9ae83aef084bc
Dominic Mai <Dominic Mai@DominicMai-PC>
2017-05-21 15:00:45 -0700

    Update

--- Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/GameSetupScene.java
+++ Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/GameSetupScene.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import edu.orangecoastcollege.cs272.finalproject.controller.Controller;
+import edu.orangecoastcollege.cs272.finalproject.model.Ship;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,11 +12,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-public class GameSetupScene implements Initializable{
+public class GameSetupScene implements Initializable {
 
 	private static Controller controller = Controller.getInstance();
-	
+
 	@FXML
 	private ComboBox<Character> mColCB;
 	@FXML
@@ -26,23 +28,36 @@ public class GameSetupScene implements Initializable{
 	private Button mPlaceBtn;
 	@FXML
 	private Button mPlay;
-	
-	private int mCounterOfShips = 10; 
-	
 	@FXML
-	public Object PlaceShip(){
-		
-		boolean place = controller.addShip(mColCB.getSelectionModel().getSelectedItem(), mRolCB.getSelectionModel().getSelectedIndex(), true);	
+	private ListView<Ship> mShips;
 
-		if(place)
+	private int mCounterOfShips = 10;
+
+	@FXML
+	public Object PlaceShip() {
+
+		boolean place = controller.addShip(mColCB.getSelectionModel().getSelectedItem(),
+				mRolCB.getSelectionModel().getSelectedIndex(), true);
+
+		if (place) {
 			mCounterOfShips--;
+			mShips.setItems(controller.getShips(true));
+		}
+
+		return this;
+	}
+
+	@FXML
+	public Object remove(){
+
+		controller.removeShip(mShips.getSelectionModel().getSelectedItem());
 		
 		return this;
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		ObservableList<Character> letters = FXCollections.observableArrayList();
 		letters.add('A');
 		letters.add('B');
@@ -54,21 +69,21 @@ public class GameSetupScene implements Initializable{
 		letters.add('H');
 		letters.add('I');
 		letters.add('j');
-		
+
 		mColCB.setItems(letters);
 		mColCB.getSelectionModel().select('A');
-		
+
 		ObservableList<Integer> numbers = FXCollections.observableArrayList();
-		for(int i = 1; i < 11; i++){
+		for (int i = 1; i < 11; i++) {
 			numbers.add(i);
 		}
 		mRolCB.setItems(numbers);
 		mRolCB.getSelectionModel().select(1);
-		
+
 		mNumOfShips.setText("10");
-		
+
 		mPlay.setDisable(true);
-		
+
 	}
-	
+
 }
