77d5aa55966a52d98b715aa1b7ced952bbb7946e
gundyne <dathuydoan@gmail.com>
2017-05-22 10:41:28 -0700

    Now updates whenever scene is loaded.

--- Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/AIChoiceScene.java
+++ Final project Battleships/src/edu/orangecoastcollege/cs272/finalproject/view/AIChoiceScene.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 import edu.orangecoastcollege.cs272.finalproject.controller.Controller;
+import edu.orangecoastcollege.cs272.finalproject.model.Missile;
 import edu.orangecoastcollege.cs272.finalproject.model.Ship;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,58 +14,91 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-public class AIChoiceScene implements Initializable {
-
+public class MissileLaunchScene implements Initializable{
+	
 	private static Controller controller = Controller.getInstance();
-	private int rowChoice, colChoice;
-	private Random rNGMissile = new Random();
-	private boolean fireLucky = controller.getDifficulty() != 0 && rNGMissile.nextBoolean();
 	
 	@FXML
-	private Label choiceMessage;
 	private GridPane playerBoard;
+	@FXML
+	private Label launchMessage;
 	
-	private int[] selectInRange(int origX, int origY)
+	@FXML
+	public Object continueGame()
+	{
+		ViewNavigator.loadScene("AI's Turn", ViewNavigator.AI_CHOICE_SCENE);
+		return this;
+	}
+
+	private int[] selectInRange(int origX, int origY, int outRad, Random rNG)
 	{
 		int[] rangeDiff = {0,0};
 		do{
-			rangeDiff[0] = rNGMissile.nextInt(5)-2;
-			rangeDiff[1] = rNGMissile.nextInt(5)-2;
+			rangeDiff[0] = rNG.nextInt(2*outRad+1)-outRad;
+			rangeDiff[1] = rNG.nextInt(2*outRad+1)-outRad;
 		}while(origX+rangeDiff[0] >=0 && origX+rangeDiff[0] <10 &&
 				origY+rangeDiff[1] >=0 && origY+rangeDiff[1] <10);
 		return rangeDiff;
 	}
 	
-	private static ObservableList<Ship> playerActiveShips()
+	private ObservableList<Ship> aIActiveShips()
 	{
-		ObservableList<Ship> filtered = FXCollections.observableArrayList();
-		ObservableList<Ship> allPlayerShips = controller.getShips(true);
-		for(Ship boat: allPlayerShips)
+		ObservableList<Ship> aiShips = controller.getShips(true);
+		ObservableList<Ship> active = FXCollections.observableArrayList();
+		for(Ship boat:aiShips)
 			if(!boat.isDestroy())
-				filtered.add(boat);
-		return filtered;
+				active.add(boat);
+		return active;
 	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		if(controller.getDifficulty() != 2)
+		launchMessage.setText("");
+		ObservableList<Missile> playerMissiles = controller.getMissilesLaunched(true);
+		Missile lastMissile = playerMissiles.get(playerMissiles.size()-1);
+		boolean luckyMissile = lastMissile.isLucky(); 
+		boolean targetHit = false;
+		ObservableList<Ship> aiActive = aIActiveShips();
+		Ship shipSelect = null;
+		for(Ship boat: aiActive)
 		{
-		do{
-			rowChoice = rNGMissile.nextInt(10);
-			colChoice = rNGMissile.nextInt(10);
-		}while(rowChoice >=0 && rowChoice <10 && colChoice >=0 && colChoice <10);
+			if(boat.getAphaCol()==lastMissile.getAphaCol() && 
+					boat.getNumRol()==lastMissile.getNumRol() &&
+					!boat.isDestroy())
+			{
+				targetHit = controller.wreckShip(shipSelect);
+				break;
+			}
+		}
+		
+		if(luckyMissile)
+		{
+			Random rNG = new Random();
+			int extraMissiles = rNG.nextInt(3)+1;
+			int hit = 0;
+			for(int m=0;m<extraMissiles;m++)
+			{
+				int[] location = selectInRange(lastMissile.getAphaCol()-'A', lastMissile.getNumRol()-1, 1, rNG);
+				for(Ship boat: aiActive)
+				{
+					if(boat.getAphaCol() == lastMissile.getAphaCol()+location[0] &&
+							boat.getNumRol() == lastMissile.getNumRol()+location[1]);
+					{
+						controller.wreckShip(boat);
+						hit++;
+						break;
+					}
+				}
+				launchMessage.setText("The AI fired a lucky missile at "+lastMissile.getAphaCol()+
+						lastMissile.getNumRol()+". "+String.valueOf(extraMissiles)+" more missiles launched with "+
+						String.valueOf(hit)+" of them as hits.");
+			}
 		}
 		else
 		{
-			ObservableList<Ship> shipsUp = playerActiveShips();
-			Ship selectedShip = shipsUp.get(rNGMissile.nextInt(shipsUp.size()));
-			int[] targetedLocation = selectInRange(selectedShip.getAphaCol()-'A', selectedShip.getNumRol());
-			rowChoice = targetedLocation[0];
-			colChoice = targetedLocation[1];
+			launchMessage.setText("The AI fired a missile at "+lastMissile.getAphaCol()+
+					lastMissile.getNumRol()+". It was a "+(targetHit?"hit":"miss")+".");
 		}
-		String colAlpha = "ABCDEFGHIJ";
-		controller.addMissile(colAlpha.charAt(colChoice), rowChoice+1, false, fireLucky);
-		int extraMissiles = rNGMissile.nextInt(4);
+		playerBoard = ViewNavigator.generateBoard(false);
 	}
-
 }
